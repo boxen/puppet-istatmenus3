a7c690a5322e9eac8546bd1a74029b53cb68c7d3
James Fryman <james@frymanet.com>
2012-10-17 14:42:45 -0700

    Initial Commit

---
+++ manifests/init.pp
@@ -0,0 +1,11 @@
+# Public: Install iStatMenus3.app to /Applications.
+#
+# Examples
+#
+#   include istatmenus3
+class istatmenus3 {
+  package { 'iStatMenus3':
+    provider => 'compressed_app',
+    source   => "http://s3.amazonaws.com/bjango/files/istatmenus3/istatmenus3.25.zip",
+  }
+}
---
+++ spec/classes/istatmenus3_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'istatmenus3' do
+  it do
+    should contain_package('iStatMenus3').with({
+      :provider => 'compressed_app',
+      :source   => 'http://s3.amazonaws.com/bjango/files/istatmenus3/istatmenus3.25.zip',
+    })
+  end
+end
